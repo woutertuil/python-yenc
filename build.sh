a25d5858318c300dfef15236cea75aa7e1c09824
Ricardo Padilha <ricardospadilha@gmail.com>
2014-10-02 17:17:45 +0200

    Fixes call to xpython2 in build script

--- build.sh
+++ build.sh
@@ -51,7 +51,7 @@ local XPYTHON=~/xtools/python2/${DROBO}
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
 sed -i -e "s|from distutils.core import setup, Extension|from setuptools import setup\nfrom distutils.core import Extension|g" setup.py
-_PYTHON_HOST_PLATFORM="linux-armv7l" PYTHONPATH="${XPYTHON}" "${XPYTHON}/bin/python" setup.py build_ext --include-dirs="${XPYTHON}/include-${DROBO}" --library-dirs="${XPYTHON}/lib-${DROBO}" --force build --force bdist_egg --dist-dir ../..
+_PYTHON_HOST_PLATFORM="linux-armv7l" LDSHARED="${CC} -shared -Wl,-rpath,/mnt/DroboFS/Share/DroboApps/python2/lib -L${DEST}/lib-5n" "${XPYTHON}/bin/python" setup.py build_ext --include-dirs="${XPYTHON}/include-${DROBO}" --library-dirs="${XPYTHON}/lib-${DROBO}" --force build --force bdist_egg --dist-dir ../..
 popd
 }
 
