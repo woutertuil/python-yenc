406e42e252ade9913a603e3c0b3975be195a95e0
Ricardo Padilha <ricardospadilha@gmail.com>
2015-06-26 18:38:40 -0700

    Compiled with python 2.7.10

--- app.sh
+++ app.sh
@@ -4,12 +4,18 @@ local VERSION="0.4.0"
 local FOLDER="yenc-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
 local URL="http://www.golug.it/pub/yenc/${FILE}"
-local XPYTHON=~/xtools/python2/${DROBO}
+local XPYTHON="${HOME}/xtools/python2/${DROBO}"
+local BASE="${PWD}"
+export QEMU_LD_PREFIX="${TOOLCHAIN}/${HOST}/libc"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
-sed -i -e "s|from distutils.core import setup, Extension|from setuptools import setup\nfrom distutils.core import Extension|g" setup.py
-_PYTHON_HOST_PLATFORM="linux-armv7l" LDSHARED="${CC} -shared -Wl,-rpath,/mnt/DroboFS/Share/DroboApps/python2/lib -L${DEST}/lib-5n" "${XPYTHON}/bin/python" setup.py build_ext --include-dirs="${XPYTHON}/include-${DROBO}" --library-dirs="${XPYTHON}/lib-${DROBO}" --force build --force bdist_egg --dist-dir ../..
+sed -e "s|from distutils.core import setup, Extension|from setuptools import setup\nfrom distutils.core import Extension|g" -i setup.py
+LDSHARED="${CC} -shared -Wl,-rpath,/mnt/DroboFS/Share/DroboApps/python2/lib -L${XPYTHON}/lib" \
+  "${XPYTHON}/bin/python" setup.py build_ext \
+  --include-dirs="${XPYTHON}/include" --library-dirs="${XPYTHON}/lib" \
+  --force build --force bdist_egg --dist-dir "${BASE}"
+#_PYTHON_HOST_PLATFORM="linux-armv7l" LDSHARED="${CC} -shared -Wl,-rpath,/mnt/DroboFS/Share/DroboApps/python2/lib -L${DEST}/lib-5n" "${XPYTHON}/bin/python" setup.py build_ext --include-dirs="${XPYTHON}/include-${DROBO}" --library-dirs="${XPYTHON}/lib-${DROBO}" --force build --force bdist_egg --dist-dir ../..
 popd
 }
 
