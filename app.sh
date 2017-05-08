a46e2afb38b6d1f95a0e925ad777961e1dbce866
woutertuil <w.tuil@republik.nl>
2017-05-08 22:41:35 +0200

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,15 +1,13 @@
 ### YENC ###
 _build_yenc() {
-local VERSION="0.4.0"
-local FOLDER="yenc-${VERSION}"
-local FILE="${FOLDER}.tar.gz"
-local URL="http://www.golug.it/pub/yenc/${FILE}"
+
 local XPYTHON="${HOME}/xtools/python2/${DROBO}"
 local BASE="${PWD}"
 export QEMU_LD_PREFIX="${TOOLCHAIN}/${HOST}/libc"
 
-_download_tgz "${FILE}" "${URL}" "${FOLDER}"
-pushd "target/${FOLDER}"
+git clone https://github.com/sabnzbd/sabyenc.git
+
+pushd "sabyenc"
 sed -e "s|from distutils.core import setup, Extension|from setuptools import setup\nfrom distutils.core import Extension|g" -i setup.py
 PKG_CONFIG_PATH="${XPYTHON}/lib/pkgconfig" \
   LDFLAGS="${LDFLAGS:-} -Wl,-rpath,/mnt/DroboFS/Share/DroboApps/python2/lib -L${XPYTHON}/lib" \
